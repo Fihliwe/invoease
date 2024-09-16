59c18684571bef730dd00e787d10e0c7e2b12c29
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-09-16 16:28:37 +0000

    Pending changes exported from your codespace

---
+++ models/User.js
@@ -0,0 +1,13 @@
+// models/User.js
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: { type: String },
+  name: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.models.User || mongoose.model('User', userSchema);
+
+export default User;
